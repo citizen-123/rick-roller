ee82e0c07aa611773701406281584754ba7d3ede
Citizen1 <daltonhrrll@gmail.com>
2023-04-16 23:55:23 -0500

    Update roller.sh

--- roller.sh
+++ roller.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # specify the URL of the .gif file to download
-gif_url="https://github.com/citizen-123/rick-roller/raw/main/ascii-art-rick-astley--444392.gif"
-
+gif_url="https://github.com/citizen-123/rick-roller/raw/main/ascii-art-rick-astley--444392.>
 # specify the file name for the downloaded .gif
 gif_file="file.gif"
 
 # use wget to download the .gif file
 wget -O "$gif_file" "$gif_url"
 
-# use xdg-open to open and play the .gif file
-xdg-open "$gif_file"
+# define an array of web browser commands to check
+browser_commands=("google-chrome" "firefox" "chromium-browser" "opera" "safari")
+
+# loop through the browser commands and check if they are installed
+for cmd in "${browser_commands[@]}"
+do
+  if command -v "$cmd" >/dev/null 2>&1 ; then
+    # if a browser is found, open the HTML file with it and exit the loop
+    "$cmd" --new-window --kiosk --start-fullscreen "file://$PWD/$gif_file"
+    exit
+  fi
+done
+
+# if no browser is found, print an error message
+echo "No web browser found."
